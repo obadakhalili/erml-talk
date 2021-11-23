74867611183faa89012de900047a2493d1f9fbae
Obada Khalili <obada.akhalili@gmail.com>
2021-11-23 11:03:21 +0200

    add speaker view plugin

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import Reveal from "reveal.js"
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js"
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js"
+import Notes from "reveal.js/plugin/notes/notes.esm"
 
 import "./styles.css"
 
-let deck = new Reveal({ hash: true, plugins: [Markdown, RevealHighlight] })
+let deck = new Reveal({ hash: true, plugins: [Markdown, RevealHighlight, Notes] })
 deck.initialize()
